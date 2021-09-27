17083672498f3d89687f5a1f5530024472f4fa0a
Patrick Nyskjold <patr447g@edu.easj.dk>
2021-09-27 11:51:32 +0200

    Added ModelLib with FootballPlayer class

---
+++ Assignment4SimpleRESTService/ModelLib/Model/FootballPlayer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLib.Model
+{
+    public class FootballPlayer
+    {
+        public int _id;
+        private string _name;
+        private int _price;
+        private int _shirtNumber;
+
+        public FootballPlayer()
+        {
+
+        }
+
+        public FootballPlayer(int id, string name, int price, int shirtNumber)
+        {
+
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length < 4) throw new ArgumentOutOfRangeException("Name", Name, "Name must be 4 characters or longer");
+                _name = value;
+            }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("Price", Price, "Price must be above 0");
+                _price = value;
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get { return _shirtNumber; }
+            set
+            {
+                if (value < 1 || value > 100) throw new ArgumentOutOfRangeException("Shirt Number", ShirtNumber, "Shirt Number must be between 1 and 100");
+                _shirtNumber = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return
+                $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(Price)}: {Price}, {nameof(ShirtNumber)}: {ShirtNumber}";
+        }
+    }
+}
